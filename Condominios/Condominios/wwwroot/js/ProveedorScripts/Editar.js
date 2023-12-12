bbee596157c8a2fcd606d9a7127132fe1a67a84a
CarlosIvan2003 <carlosivan12.ci@gmail.com>
2023-12-11 23:33:45 -0600

    CHC_007_Desplegables

--- Condominios/Condominios/wwwroot/js/ProveedorScripts/Editar.js
+++ Condominios/Condominios/wwwroot/js/ProveedorScripts/Editar.js
@@ -2,41 +2,59 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    var BtnEditar = document.querySelectorAll('.remover');
+
+    // Datos del formulario
     var Formulario = document.querySelector("form");
     var BotonEnviar = Formulario.querySelector('[type="submit"]');
+    var BtnEditar = document.querySelectorAll('.remover');
+
+    // Datos del desplegable
+    var Desplegable = document.getElementById('form');
+    var FlechaAbajo = document.getElementById('down');
+    var FlechaArriba = document.getElementById('up');
+
+    // Bandera
+
+    var UltimoBotonPresionado = null;
 
     BtnEditar.forEach(function (element) {
         element.addEventListener('click', function (event) {
+
             var Parametro = event.currentTarget.getAttribute('data-parametro');
-            var Desplegable = document.getElementById('form');
-            var FlechaAbajo = document.getElementById('down');
-            var FlechaArriba = document.getElementById('up');
 
-            if (BotonEnviar.value == "Agregar") {
-                ConsultaGET(Parametro);
+            if (UltimoBotonPresionado === Parametro) {
+                CerrarDesplegable();
+                UltimoBotonPresionado = null;
+                Formulario.reset();
             } else {
-                BotonEnviar.value = "Agregar";
-            }
-
-            if (!Desplegable.classList.contains('contract') && !Desplegable.classList.contains('expanded')) {
-                Desplegable.classList.add('expanded');
-                FlechaAbajo.classList.add('d-none');
-                FlechaArriba.classList.remove('d-none');
-            } else if (Desplegable.classList.contains('expanded')) {
-                borrarDatosObtenidos();
-                Desplegable.classList.remove('expanded');
-                Desplegable.classList.add('contract');
-                FlechaAbajo.classList.remove('d-none');
-                FlechaArriba.classList.add('d-none');
-            } else if (Desplegable.classList.contains('contract')) {
-                Desplegable.classList.remove('contract');
-                Desplegable.classList.add('expanded');
-                FlechaAbajo.classList.add('d-none');
-                FlechaArriba.classList.remove('d-none');
+                AbrirDesplegable();
+                UltimoBotonPresionado = Parametro;
+                ConsultaGET(Parametro);
             }
         });
     });
+
+    function AbrirDesplegable() {
+        if (!Desplegable.classList.contains('contract') && !Desplegable.classList.contains('expanded')) {
+            Desplegable.classList.add('expanded');
+            FlechaAbajo.classList.add('d-none');
+            FlechaArriba.classList.remove('d-none');
+        } else if (Desplegable.classList.contains('contract')) {
+            Desplegable.classList.remove('contract');
+            Desplegable.classList.add('expanded');
+            FlechaAbajo.classList.add('d-none');
+            FlechaArriba.classList.remove('d-none');
+        }
+    }
+
+    function CerrarDesplegable() {
+        Desplegable.classList.remove('expanded');
+        Desplegable.classList.add('contract');
+        FlechaAbajo.classList.remove('d-none');
+        FlechaArriba.classList.add('d-none');
+        Formulario.action = "/Proveedores/Agregar";
+        BotonEnviar.value = "Agregar";
+    }
 
     function ConsultaGET(Parametro) {
         api.get(`Proveedores/ObtenerRegistro/${Parametro}`)
@@ -53,12 +71,9 @@ document.addEventListener('DOMContentLoaded', () => {
             })
             .catch(error => console.error('GET Error:', error));
     }
-
-    function borrarDatosObtenidos() {
-        document.getElementById("ID").value = "";
-        document.getElementById("Nombre").value = "";
-        document.getElementById("Numero").value = "";
-        document.getElementById("Direccion").value = "";
-        document.getElementById("Correo").value = "";
-    }
+    FlechaArriba.addEventListener('click', function () {
+        Formulario.reset();
+        Formulario.action = "/Proveedores/Agregar";
+        BotonEnviar.value = "Agregar";
+    });
 });
--- Condominios/Condominios/wwwroot/js/TipoEquiposScripts/Editar.js
+++ Condominios/Condominios/wwwroot/js/TipoEquiposScripts/Editar.js
@@ -1,49 +1,70 @@
 ﻿const api = new ApiClient();
 
+
+
+
+
+
 //Abrir despegable
 document.addEventListener('DOMContentLoaded', () => {
 
-    var BtnEditar = document.querySelectorAll('.remover');
-
+    // Datos del formulario
     var ContenedorDiv = document.getElementById("form-body");
     var Formulario = ContenedorDiv.querySelector("form");
     var BotonEnviar = Formulario.querySelector('[type="submit"]');
+    var BtnEditar = document.querySelectorAll('.remover');
+
+    // Datos del desplegable
+    var Desplegable = document.getElementById('form');
+    var FlechaAbajo = document.getElementById('down');
+    var FlechaArriba = document.getElementById('up');
+
+    // Bandera
+    var UltimoBotonPresionado = null;
 
     BtnEditar.forEach(function (element) {
         element.addEventListener('click', function (event) {
+
             var Parametro = event.currentTarget.getAttribute('data-parametro');
-            var Desplegable = document.getElementById('form');
-            var FlechaAbajo = document.getElementById('down');
-            var FlechaArriba = document.getElementById('up');
 
-            if (BotonEnviar.value == "Agregar") {
-                ConsultaGET(Parametro);
+            if (UltimoBotonPresionado === Parametro) {
+                CerrarDesplegable();
+                UltimoBotonPresionado = null;
+                Formulario.reset();
             } else {
-                BotonEnviar.value = "Agregar";
-            }
-
-            if (!Desplegable.classList.contains('contract') && !Desplegable.classList.contains('expanded')) {
-                Desplegable.classList.add('expanded');
-                FlechaAbajo.classList.add('d-none');
-                FlechaArriba.classList.remove('d-none');
-            } else if (Desplegable.classList.contains('expanded')) {
-                borrarDatosObtenidos();
-                Desplegable.classList.remove('expanded');
-                Desplegable.classList.add('contract');
-                FlechaAbajo.classList.remove('d-none');
-                FlechaArriba.classList.add('d-none');
-            } else if (Desplegable.classList.contains('contract')) {
-                Desplegable.classList.remove('contract');
-                Desplegable.classList.add('expanded');
-                FlechaAbajo.classList.add('d-none');
-                FlechaArriba.classList.remove('d-none');
+                AbrirDesplegable();
+                UltimoBotonPresionado = Parametro;
+                ConsultaGET(Parametro);
             }
         });
     });
 
+    function AbrirDesplegable() {
+        if (!Desplegable.classList.contains('contract') && !Desplegable.classList.contains('expanded')) {
+            Desplegable.classList.add('expanded');
+            FlechaAbajo.classList.add('d-none');
+            FlechaArriba.classList.remove('d-none');
+        } else if (Desplegable.classList.contains('contract')) {
+            Desplegable.classList.remove('contract');
+            Desplegable.classList.add('expanded');
+            FlechaAbajo.classList.add('d-none');
+            FlechaArriba.classList.remove('d-none');
+        }
+    }
+
+    function CerrarDesplegable() {
+        Desplegable.classList.remove('expanded');
+        Desplegable.classList.add('contract');
+        FlechaAbajo.classList.remove('d-none');
+        FlechaArriba.classList.add('d-none');
+        Formulario.action = "/TipoEquipos/Agregar";
+        BotonEnviar.value = "Agregar";
+    }
+
     function ConsultaGET(Parametro) {
         api.get(`TipoEquipos/ObtenerRegistro/${Parametro}`)
             .then(data => {
+
                 var CapacidadValue = data.capacidad;
                 var CapacidadInt = parseInt(CapacidadValue);
                 var CapacidadString = CapacidadValue.replace(/[0-9\s]/g, '');
@@ -73,15 +94,10 @@ document.addEventListener('DOMContentLoaded', () => {
             .catch(error => console.error('GET Error:', error));
     }
 
-
-    function borrarDatosObtenidos() {
-        document.getElementById("VarianteEquipo_MarcaID").value = "";
-        document.getElementById("VarianteEquipo_MotorID").value = "";
-        document.getElementById("VarianteEquipo_PeriodoID").value = "";
-        document.getElementById("VarianteEquipo_TipoEquipoID").value = "";
-        document.getElementById("VarianteEquipo_CapacidadValor").value = "";
-    }
+    FlechaArriba.addEventListener('click', function () {
+        Formulario.reset();
+        Formulario.action = "/TipoEquipos/Agregar";
+        BotonEnviar.value = "Agregar";
+    });
 });
-
-
 

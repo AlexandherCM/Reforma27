c1e13ce78a96926e2901d9bd3db69b9bf720342a
AlexandherCM <alexandher_1321124129@uptecamac.edu.mx>
2023-12-18 16:41:01 -0600

    CHA_0020_CHB0013

--- Condominios/Condominios/wwwroot/js/Equipos/DetailsMto.js
+++ Condominios/Condominios/wwwroot/js/Equipos/DetailsMto.js
@@ -1,30 +1,39 @@
-﻿let api = new ApiClient(); 
-let parametro;
-let btnsDetails = document.getElementsByClassName('btn-details');
-let btnConsultarMto = document.getElementById('btn-ConsultarMtos');
+﻿//let api = new ApiClient(); 
+//let parametro;
+//let btnConsultarMto = document.getElementById('btn-ConsultarMtos');
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+let btnsDetails = document.getElementsByClassName('btn-details');
+
 Array.from(btnsDetails).forEach(btn => {
     btn.addEventListener('click', () => {
-        parametro = btn.getAttribute("data-parametro");
+        let parametro = btn.getAttribute("data-parametro");
 
-        api.get(`Mantenimientos/GetMtoProgramado/${parametro}`, { timeout: 10000 })
-            .then(data => {
-                console.log(`ID: ${parametro}`);
-                console.log(data);
-            })
-            .catch(error => console.error('Get Error:', error));
-
-        contenedor.classList.toggle('mostrarDown');
-        MostrarMas.src = contenedor.classList.contains('mostrarDown') ? '/images/down.svg' : '../../images/up.svg';
+        window.location.href = `/Mantenimientos/Consultar/${parametro}`;
     });
 });
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//Array.from(btnsDetails).forEach(btn => {
+//    btn.addEventListener('click', () => {
+//        parametro = btn.getAttribute("data-parametro");
+
+//        api.get(`Mantenimientos/GetMtoProgramado`)
+//            .then(data => {
+//                console.log(data);
+//            })
+//            .catch(error => console.error('POST Error:', error));
+
+//        contenedor.classList.toggle('mostrarDown');
+//        MostrarMas.src = contenedor.classList.contains('mostrarDown') ? '/images/down.svg' : '../../images/up.svg';
+//    });
+//});
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-btnConsultarMto.addEventListener('click', () => {
-    if (parametro)
-        window.location.href = `/Mantenimientos/Consultar/${parametro}`;
-});
+//btnConsultarMto.addEventListener('click', () => {
+//    if (parametro)
+//        window.location.href = `/Mantenimientos/Consultar/${parametro}`;
+//});
 
 
 
